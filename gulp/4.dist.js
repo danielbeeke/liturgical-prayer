96c187f8b8c2ecf2b30315c51b6e08a735dd28b6
Daniel Beeke <mail@danielbeeke.nl>
2020-05-13 23:00:05 +0200

    100vh to 100%

--- gulp/4.dist.js
+++ gulp/4.dist.js
@@ -1,7 +1,21 @@
 'use strict';
 
 const gulp = require('gulp');
+const clean = require('gulp-clean');
+const rename = require('gulp-rename');
 
-gulp.task('copy', () => {
+gulp.task('clean', () => {
+  return gulp.src(['dist/*']).pipe(clean({read: false}));
+});
+
+gulp.task('copy-dist', () => {
   return gulp.src(['public/**/*']).pipe(gulp.dest('dist'));
 });
+
+gulp.task('create-404', () => {
+  return gulp.src(['public/index.html'])
+  .pipe(rename('404.html'))
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy', gulp.series(['clean', 'copy-dist', 'create-404']));
--- gulp/5.deploy.js
+++ gulp/5.deploy.js
@@ -1,7 +1,9 @@
 const gulp = require('gulp');
 const ghPages = require('gulp-gh-pages');
 
-gulp.task('deploy', function() {
+gulp.task('github', function() {
   return gulp.src('./dist/**/*')
     .pipe(ghPages());
 });
+
+gulp.task('deploy', gulp.series(['copy', 'github']));
