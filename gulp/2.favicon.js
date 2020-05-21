77e95491727dfc3f75312f4d65c95a1be0e4ed07
Daniel Beeke <mail@danielbeeke.nl>
2020-05-21 22:50:11 +0200

    more fixes

--- gulp/2.favicon.js
+++ gulp/2.favicon.js
@@ -4,7 +4,7 @@ const gulp = require('gulp');
 const exec = require('child_process').exec;
 
 gulp.task('favicon', function (cb) {
-  exec('pwa-asset-generator public/images/logo.svg -i ./public/index.html -m ./public/manifest.webmanifest ./public', function (err, stdout, stderr) {
+  exec('pwa-asset-generator public/images/logo.svg -i ./public/index.html -t png -m ./public/manifest.json ./public', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     cb(err);
