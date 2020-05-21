ec3160dc8e1473f25ba83408b82b908f4db0ed37
Daniel Beeke <mail@danielbeeke.nl>
2020-05-21 21:57:22 +0200

    Adding favicon generator

--- gulp/2.favicon.js
+++ gulp/2.favicon.js
@@ -1,33 +1,12 @@
 'use strict';
 
 const gulp = require('gulp');
-const webshot = require('gulp-webshot');
+const exec = require('child_process').exec;
 
-gulp.task('favicon', function() {
-  return gulp.src('./public/favicon.html')
-  .pipe(webshot({ dest: 'public/favicon/', root:'public' }));
-});
-
-// const sizes = [
-//   { name: 'favicon-16', size: '16/16' },
-//   { name: 'favicon-32', size: '32/32' },
-//   { name: 'apple-icon-180', size: '180/180' },
-//   { name: 'pwa-icon-192', size: '192/192' },
-//   { name: 'pwa-icon-512', size: '512/512' },
-//   { name: 'apple-splash-640', size: '640/1136' },
-//   { name: 'apple-splash-750', size: '750/1334' },
-//   { name: 'apple-splash-1242', size: '1242/2208' },
-//   { name: 'apple-splash-1125', size: '1125/2436' },
-//   { name: 'apple-splash-1125', size: '1125/2436' },
-//   { name: 'apple-splash-1536', size: '1536/2048' },
-//   { name: 'apple-splash-1668', size: '1668/2224' },
-//   { name: 'apple-splash-2048', size: '2048/2732' },
-// ];
-//
-// sizes.map(item => {
-//   exec(util.format(
-//     'npx webshot --window-size=%s ./favicon.html ./public/favicon/%s.png',
-//     item.size,
-//     item.name
-//   ))
-// });
+gulp.task('favicon', function (cb) {
+  exec('pwa-asset-generator public/images/logo.svg -i ./public/index.html -m ./public/manifest.webmanifest ./public', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+})
