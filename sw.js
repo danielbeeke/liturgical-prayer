ef68ea7cd5bc4a59c9eae305772c0dce591d7ff9
Daniel Beeke <mail@danielbeeke.nl>
2020-06-01 11:28:41 +0200

    Update 2020-06-01T09:28:39.391Z

---
+++ sw.js
@@ -0,0 +1,14 @@
+let cacheName = 'liturgical-prayer-v1';
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+      return r || fetch(e.request).then((response) => {
+        return caches.open(cacheName).then((cache) => {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
